4ce33b8950692796d9015b441c3aec0582f4f86b
BigBoomBox <108496204+BigBoomBox@users.noreply.github.com>
2022-07-21 17:33:23 +0200

    Remake

--- app/user/user.py
+++ app/user/user.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template, request
-from utils import get_users_posts
+from utils import get_posts_by_user
 
 user_blueprint = Blueprint('user_blueprint', __name__, template_folder="templates")
 
 
-@user_blueprint.route("/posts/<username>")
+@user_blueprint.route('/posts/<username>')
 def users(username):
-    user_posts = get_users_posts(username)
-    user_name = user_posts[0]["poster_name"]
-    return render_template('user-feed.html', users_posts=user_posts, user_name=user_name)
+    users_posts = get_posts_by_user(username)
+    user_name = users_posts[0]["poster_name"]
+    return render_template('user-feed.html', users_posts=users_posts, user_name=user_name)
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from utils import get_posts, get_posts_by_pk
+from utils import get_posts_all, get_post_by_pk
 from app.index.views import index_blueprint
 from app.search.search import search_blueprint
 from app.user.user import user_blueprint
@@ -13,13 +13,13 @@ app.register_blueprint(user_blueprint)
 
 @app.route("/api/posts")
 def api_1():
-    data = get_posts()
+    data = get_posts_all()
     return jsonify(data)
 
 
 @app.route("/api/posts/<int:post_id>")
 def api_2(post_id):
-    post = get_posts_by_pk(post_id)
+    post = get_post_by_pk(post_id)
     return jsonify(post)
 
 
